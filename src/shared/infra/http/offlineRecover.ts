519557e97581ef11a8b1fe1de31255a5c7a72be6
fsegall <segall.felipe@gmail.com>
2022-04-13 09:35:39 -0300

    Write file to disk

--- src/shared/infra/http/offlineRecover.ts
+++ src/shared/infra/http/offlineRecover.ts
@@ -1,6 +1,5 @@
 import { Router } from 'express';
-
-
+import fs from 'fs';
 import ensureAuthenticated from '@modules/users/infra/http/middlewares/ensureAuthenticated'
 
 
@@ -12,6 +11,7 @@ offlineRouter.post(
   (req, res) => {
     const interviews = {interviews: req.body.interviews}
     console.log(interviews)
+    fs.writeFileSync('./martha.json', JSON.stringify(req.body.interviews), 'utf8' );
     res.status(200).json(interviews)
   },
 );
