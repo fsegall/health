932d261d230ac5fcd9ec417e1d2da210b3b68c2e
Amanda Manduca <amandavmanduca@hotmail.com>
2023-01-08 15:35:10 -0300

    :sparkles: Implements indigenous offline attempt functionality with invalid response

--- src/modules/indigenous/services/HandleOfflineInterviewsService.ts
+++ src/modules/indigenous/services/HandleOfflineInterviewsService.ts
@@ -2,8 +2,10 @@ import { writeFileSync, mkdirSync, existsSync } from 'fs';
 import { join } from 'path';
 import { inject, injectable } from 'tsyringe';
 
-import Project from '../../projects/infra/typeorm/entities/Project';
+import AppError from '@shared/errors/AppError';
 
+import Project from '../../projects/infra/typeorm/entities/Project';
+import ProjectsRepository from '../../projects/infra/typeorm/repositories/ProjectsRepository';
 import { IHandleOfflineInterviewsDTO } from '../dtos/IHandleOfflineInterviewsDTO';
 import { IIndigenousAlimentacaoNutricaoRepository } from '../repositories/IIndigenousAlimentacaoNutricaoRepository';
 import { IIndigenousApoioEProtecaoRepository } from '../repositories/IIndigenousApoioEProtecaoRepository';
@@ -11,9 +13,6 @@ import { IIndigenousInterviewDemographyRepository } from '../repositories/IIndig
 import { IIndigenousInterviewRepository } from '../repositories/IIndigenousInterviewRepository';
 import { IIndigenousInterviewResidenceRepository } from '../repositories/IIndigenousInterviewResidenceRepository';
 import { IIndigenousSaudeDoencaRepository } from '../repositories/IIndigenousSaudeDoencaRepository';
-
-import ProjectsRepository from '../../projects/infra/typeorm/repositories/ProjectsRepository'
-import AppError from '@shared/errors/AppError';
 
 @injectable()
 export class HandleOfflineInterviewsService {
@@ -42,9 +41,9 @@ export class HandleOfflineInterviewsService {
 
   private createOfflineRequestBackup(data: IHandleOfflineInterviewsDTO[]) {
     const backupFileName = `backup-indigenousInterview-${new Date().getTime()}`;
-    const dirPath = `src/backups`
+    const dirPath = `src/backups`;
     if (!existsSync(dirPath)) {
-      mkdirSync(dirPath, { recursive: true })
+      mkdirSync(dirPath, { recursive: true });
     }
     writeFileSync(
       join(process.cwd(), `${dirPath}/${backupFileName}.json`),
@@ -53,50 +52,67 @@ export class HandleOfflineInterviewsService {
     );
   }
 
-  async execute(data: IHandleOfflineInterviewsDTO[]) {
+  async execute(data: IHandleOfflineInterviewsDTO[]): Promise<any> {
     this.createOfflineRequestBackup(data);
-    const interviewsToSave = Object.values(data[0]).map(async interview => {
+    let notSavedInterviews: any = {};
+    await Promise.all(
+      data?.map(
+        async i =>
+          // eslint-disable-next-line no-return-await
+          await Promise.all(
+            Object.entries(i).map(async ([key, interview]) => {
+              const project = await this.projectsRepository.findByNumber(
+                interview.indigenous_informacoes_basicas.numero_projeto,
+              );
 
-      const project = await this.projectsRepository.findByNumber(interview.indigenous_informacoes_basicas.numero_projeto)
+              if (project === undefined) {
+                notSavedInterviews = {
+                  ...notSavedInterviews,
+                  [key]: interview,
+                };
+                console.log(
+                  `Projeto nº ${interview.indigenous_informacoes_basicas.numero_projeto.toString()} não existe.`,
+                );
+                return;
+              }
 
-      if(project === undefined) {
-        return new AppError("Esse projeto não existe.")
-      }
+              const indigenousInterview = await this.indigenousInterviewRepository.create(
+                {
+                  projeto_id: project.id,
+                  ...interview.indigenous_informacoes_basicas,
+                },
+              );
 
-      const indigenousInterview = await this.indigenousInterviewRepository.create(
-      {projeto_id: project.id,
-      ...interview.indigenous_informacoes_basicas
-      }
-      );
+              await this.indigenousInterviewDemographyRepository.create({
+                ...interview.indigenous_demografico,
+                entrevista_indigena_id: indigenousInterview.id,
+              });
 
-      await this.indigenousInterviewDemographyRepository.create({
-        ...interview.indigenous_demografico,
-        entrevista_indigena_id: indigenousInterview.id,
-      });
+              await this.indigeanousInterviewResidenceRepository.create({
+                ...interview.indigenous_domicilio,
+                veiculos: interview.indigenous_domicilio.veiculos.toString(),
+                destino_lixo_da_residencia: interview.indigenous_domicilio.destino_lixo_da_residencia.toString(),
+                entrevista_indigena_id: indigenousInterview.id,
+              });
 
-      await this.indigeanousInterviewResidenceRepository.create({
-        ...interview.indigenous_domicilio,
-        veiculos: interview.indigenous_domicilio.veiculos.toString(),
-        destino_lixo_da_residencia: interview.indigenous_domicilio.destino_lixo_da_residencia.toString(),
-        entrevista_indigena_id: indigenousInterview.id,
-      });
+              await this.indigeanousSaudeDoencaRepository.create({
+                ...interview.indigenous_saude_doenca,
+                entrevista_indigena_id: indigenousInterview.id,
+              });
 
-      await this.indigeanousSaudeDoencaRepository.create({
-        ...interview.indigenous_saude_doenca,
-        entrevista_indigena_id: indigenousInterview.id,
-      });
+              await this.indigenousAlimentacaoNutricaoRepository.create({
+                ...interview.indigenous_alimentacao_nutricao,
+                entrevista_indigena_id: indigenousInterview.id,
+              });
 
-      await this.indigenousAlimentacaoNutricaoRepository.create({
-        ...interview.indigenous_alimentacao_nutricao,
-        entrevista_indigena_id: indigenousInterview.id,
-      });
-
-      await this.indigenousApoioEProtecaoRepository.create({
-        ...interview.indigenous_apoio_protecao_social,
-        entrevista_indigena_id: indigenousInterview.id,
-      });
-    });
-
-    await Promise.all(interviewsToSave);
+              await this.indigenousApoioEProtecaoRepository.create({
+                ...interview.indigenous_apoio_protecao_social,
+                entrevista_indigena_id: indigenousInterview.id,
+              });
+            }),
+          ),
+      ),
+    );
+    return notSavedInterviews;
   }
 }
