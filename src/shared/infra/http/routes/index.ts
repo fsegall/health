a18564d03406224dcf15906a3b4d4bb0595134d2
Raul Neto <raulneto90@gmail.com>
2023-12-10 14:19:42 -0300

    refactor: change indigeanous interview v2 routes

--- src/shared/infra/http/routes/index.ts
+++ src/shared/infra/http/routes/index.ts
@@ -3,7 +3,8 @@ import { Router } from 'express';
 import { discriminationRouter } from '@modules/discriminations/infra/http/routes/discrimination.routes';
 import addressesRouter from '@modules/households/infra/http/routes/addresses.routes';
 import householdsRouter from '@modules/households/infra/http/routes/households.routes';
-import { indigeanousInterviewRouter } from '@modules/indigenous/infra/http/routes/indigeanousInterview.routes';
+import { indigeanousInterviewRouter } from '@modules/indigenous/v1/infra/http/routes/indigeanousInterview.routes';
+import { indigeanousInterviewRouter as indigeanousInterviewRouterV2 } from '@modules/indigenous/v2/infra/http/routes/indigeanousInterview.routes';
 import interviewsRouter from '@modules/interviews/infra/http/routes/interviews.routes';
 import familyMembersRouter from '@modules/persons/infra/http/routes/familyMembers.routes';
 import personsRouter from '@modules/persons/infra/http/routes/persons.routes';
@@ -27,6 +28,7 @@ routes.use('/users', usersRouter);
 routes.use('/sessions', sessionsRouter);
 routes.use('/offline', offlineRouter);
 routes.use('/indigenous-interviews', indigeanousInterviewRouter);
+routes.use('/indigenous-interviews/v2', indigeanousInterviewRouterV2);
 routes.use('/interviews/discrimination', discriminationRouter);
 
 export default routes;
