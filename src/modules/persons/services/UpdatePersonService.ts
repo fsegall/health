e1418fba86a7e370c58fce2984536d27d053008d
Amanda Manduca <amandavmanduca@hotmail.com>
2024-03-29 15:57:44 -0300

    Fix person form

--- src/modules/persons/services/UpdatePersonService.ts
+++ src/modules/persons/services/UpdatePersonService.ts
@@ -23,11 +23,11 @@ interface IRequest {
   vacina: string;
   nao_tomou_vacina?: string;
   estado_de_saude?: string;
-  local_de_procura_do_servico_de_saude?: string;
-  motivo_procura_servico_saude?: string;
-  motivo_nao_atendimento_servico_saude?: string;
-  doenca_ultimos_12_meses?: string;
-  diagnostico_doenca_ultimos_12_meses?: string;
+  local_de_procura_do_servico_de_saude: string[];
+  motivo_procura_servico_saude: string[];
+  motivo_nao_atendimento_servico_saude?: string[];
+  doenca_ultimos_12_meses: string[];
+  diagnostico_doenca_ultimos_12_meses: string[];
 }
 
 @injectable()
